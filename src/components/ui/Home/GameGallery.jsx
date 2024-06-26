fdfd9cb1e4e4f693b94f7831337d7a384f729c71
Remon Tripura <remontripura045@gmail.com>
2024-06-26 15:19:54 +0600

    remon push

--- src/components/ui/Home/GameGallery.jsx
+++ src/components/ui/Home/GameGallery.jsx
@@ -64,11 +64,11 @@ const GameGallery = () => {
   return (
     <div className="w-full">
       <Container>
-        <div className="grid grid-cols-12 gap-5">
+        <div className="grid grid-cols-12 gap-3">
           {GameGalleryApi.map((item, i) => (
             <div
               key={i}
-              className=" h-[300px] relative md:col-span-4 col-span-12"
+              className="md:col-span-4 col-span-6 md:h-[300px] h-[150px] relative"
             >
               <Image
                 className="w-full h-full"
@@ -79,17 +79,13 @@ const GameGallery = () => {
               />
               <div className="bg-[#0000005b] absolute bottom-0 right-0 left-0">
                 <div className="relative">
-                  <h3 className="font-semibold absolute text-white py-2 left-3 top-0 bottom-0 my-auto">
+                  <h3 className="font-semibold absolute text-white py-2 left-3 top-0 bottom-0 my-auto md:text-[18px] text-[12px]">
                     Rummy
                   </h3>
-                  <div
-                    className="bg-primary py-2 flex justify-end"
-                    style={{
-                      clipPath:
-                        "polygon(75% 85%, 78% 0, 100% 0, 100% 100%, 0 100%, 0 85%)",
-                    }}
-                  >
-                    <button className="font-semibold pr-2">Play Now</button>
+                  <div className="bg-primary py-2 flex justify-end medium-game-path">
+                    <button className="font-semibold md:text-[18px] text-[12px] md:pr-4 pr-1">
+                      Play Now
+                    </button>
                   </div>
                 </div>
               </div>
